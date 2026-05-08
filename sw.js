f11d600c8c7680ff2e7d9f76a52f4fff2e0d9f08
Jakz1-creator <x7bv28txng@privaterelay.appleid.com>
2026-05-08 11:48:18 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'jacobz-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@400;500;600&display=swap'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function(){});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      if(cached) return cached;
+      return fetch(e.request).then(function(response) {
+        if(!response || response.status !== 200) return response;
+        var clone = response.clone();
+        caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
+        return response;
+      }).catch(function(){ return cached; });
+    })
+  );
+});
